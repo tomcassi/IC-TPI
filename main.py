7b44582d7e13d5694958f81ab7b1d33bbc81c146
TomCass <61022932+tomcassi@users.noreply.github.com>
2024-10-31 18:18:47 -0300

    a

--- main.py
+++ main.py
@@ -4,7 +4,21 @@ import pretty_midi
 def cargar_cancion(file_path):
     midi_data = pretty_midi.PrettyMIDI(file_path)
     return midi_data
-    
+
+
+""" 
+============ Pitch ============
+Rango 0-127
+Representa la nota que se toca, 0=C1, 127= G9.
+Cada incremento es un semitono
+En bateria o elementos de percusion no representa la nota, sino el sonido de percusion
+
+============ Velocity ============
+0-127
+Representa la fuerza con la que se toca la nota
+"""
+
+ 
 
 
 midi_data = cargar_cancion("Happy Birthday MIDI.mid")
