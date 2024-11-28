73e3dda70c58c6fc512c5bc1e98cdec8c025d9cf
Ramagpj <ramiro.gomezparis@gmail.com>
2024-11-27 23:27:04 -0300

    a

--- MLP.py
+++ MLP.py
@@ -52,6 +52,34 @@ def predecir_sig_elem_rf(elem_originales, modelo, cant_predicciones):
     return elementos
 
 
+
+def predecir_sig_elem_mlp(elem_originales, modelo, cant_predicciones):
+   
+
+    n_predicciones = 0
+    elementos = elem_originales
+    
+    while n_predicciones < cant_predicciones:
+     
+        elem_input = np.array(elementos[n_predicciones:n_predicciones + len(elem_originales)]).reshape(1, -1)
+        
+        # Hacemos la predicción usando el modelo MLP
+        prediccion = modelo.predict(elem_input)
+        
+        # La predicción se toma de la primera posición (en caso de que sea un solo valor)
+        prediccion = prediccion[0]
+        
+        # Añadimos la predicción al conjunto de elementos
+        elementos.append(prediccion)
+        
+        n_predicciones += 1
+    
+    return elementos
+
+
+
+
+
 def entrenar_modelo_rf(X, y, rf):
     # Dividir el conjunto de datos en entrenamiento y prueba
     # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05, shuffle=True, random_state=42)
@@ -122,7 +150,7 @@ def inicializar_modelo(carpeta_audios,longitud_secuencia, notasyacordes, nombre_
     return mlp_pitch, rf_velocity, rf_duration
 
 
-def predecir_cancion(rf_pitch, rf_velocity, rf_duration, longitud_secuencia, notasyacordes, cancion_inicial, nombre_pieza, cant_predicciones):
+def predecir_cancion(mlp_pitch, rf_velocity, rf_duration, longitud_secuencia, notasyacordes, cancion_inicial, nombre_pieza, cant_predicciones):
     #Predecir cancion:
     todos_caracteristicas = cargarPista(cancion_inicial, nombre_pieza)
     
@@ -135,8 +163,54 @@ def predecir_cancion(rf_pitch, rf_velocity, rf_duration, longitud_secuencia, not
     
     #predigo elementos:
     print("\n====Prediccion de Pitches====")
-    pitches_conprediccion = predecir_sig_elem_rf(todos_caracteristicas[0][0:longitud_secuencia], rf_pitch, cant_predicciones)
+    
+    pitches_conprediccion = predecir_sig_elem_mlp(todos_caracteristicas[0][0:longitud_secuencia], mlp_pitch, cant_predicciones)
     #vuelvo a transformar de indice a notas y acordes
+    for i in range(len(pitches_conprediccion)):
+        pitches_conprediccion[i] = notasyacordes[pitches_conprediccion[i]]
+    
+    
+    #===== Para velocities =====
+    print("\n====Prediccion de Velocities====")
+    velocities_conprediccion = predecir_sig_elem_rf(todos_caracteristicas[1][0:longitud_secuencia], rf_velocity, cant_predicciones)
+    
+    #===== Para durations =====
+    print("\n====Prediccion de Durations====")
+    #multiplico por 1000 y transformo en enteros
+    durations_originales = todos_caracteristicas[2][0:longitud_secuencia]
+    for i in range(len(durations_originales)):
+        durations_originales[i] *= 1000
+    durations_conprediccion = predecir_sig_elem_rf(durations_originales, rf_duration, cant_predicciones)
+    for i in range(len(durations_conprediccion)):
+        durations_conprediccion[i] /= 1000
+
+    return pitches_conprediccion, velocities_conprediccion, durations_conprediccion
+
+
+
+def predecir_cancion_random(mlp_pitch, rf_velocity, rf_duration, longitud_secuencia, notasyacordes, cant_predicciones,tempo):
+    # Generar valores iniciales aleatorios
+    duraciones_posibles = [i / 4 for i in range(5)]  # Genera [0, 0.25, 0.5, .]
+
+    # Generar valores iniciales aleatorios
+    todos_caracteristicas = [
+        [random.choice(notasyacordes) for _ in range(longitud_secuencia)],  # Pitches
+        [random.randint(0, 127) for _ in range(longitud_secuencia)],        # Velocities
+        [random.choice(duraciones_posibles) for _ in range(longitud_secuencia)],
+        tempo# Durations (en cuartos)
+    ]
+    
+        
+    #===== Para pitch =====
+    #transformo en indice de mapa:
+    for i, nota_acorde in enumerate(todos_caracteristicas[0]):
+        indice = notasyacordes.index(sorted(nota_acorde))
+        todos_caracteristicas[0][i] = indice
+    
+    # Predigo elementos
+    pitches_conprediccion = predecir_sig_elem_mlp(todos_caracteristicas[0][0:longitud_secuencia], mlp_pitch, cant_predicciones,longitud_secuencia,len(notasyacordes))
+    
+    # Vuelvo a transformar de índice a notas y acordes
     for i in range(len(pitches_conprediccion)):
         pitches_conprediccion[i] = notasyacordes[pitches_conprediccion[i]]
     
@@ -175,10 +249,6 @@ def predecir_cancion(rf_pitch, rf_velocity, rf_duration, longitud_secuencia, not
 
 
 
-
-
-
-
 if __name__ == "__main__":
     from IPython import get_ipython
     get_ipython().magic('clear')
@@ -189,7 +259,7 @@ if __name__ == "__main__":
     
     c_a = "Audios/"
 
-    cancion_a_continuar = "AudiosAux/elise.mid"
+    cancion_a_continuar = "Audios/elise.mid"
 
     
     
@@ -203,13 +273,21 @@ if __name__ == "__main__":
     # nombre_pista2 = "piano left"
     
     
-    l_s_r,l_s_l=calcular_longitud_secuencia(cancion_a_continuar, tiempo_secuencia,nombre_pista1,nombre_pista2)
+    #l_s_r,l_s_l=calcular_longitud_secuencia(cancion_a_continuar, tiempo_secuencia,nombre_pista1,nombre_pista2)
+    
+    l_s_r=15
+    l_s_l=15
+    
+    cant_predicciones_r=400
+    cant_predicciones_l = 400
     
     tempo_bpm = getTempo(cancion_a_continuar)
     firma_de_compas = getTimeSignature(cancion_a_continuar)
 
 
-    cant_predicciones_r,cant_predicciones_l=calcular_longitud_secuencia(cancion_a_continuar, tiempo_a_predecir,nombre_pista1,nombre_pista2)
+    #cant_predicciones_r,cant_predicciones_l=calcular_longitud_secuencia(cancion_a_continuar, tiempo_a_predecir,nombre_pista1,nombre_pista2)
+ 
+    
  
     
 
@@ -223,8 +301,8 @@ if __name__ == "__main__":
     mlp_p_r, rf_v_r, rf_d_r = inicializar_modelo(c_a,l_s_r, mapa_right, nombre_pista1)
     mlp_p_l, rf_v_l, rf_d_l = inicializar_modelo(c_a,l_s_l, mapa_left, nombre_pista2)
     
-    p_conprediccion_r, v_conprediccion_r, d_conprediccion_r = predecir_cancion(mlp_p_r, rf_v_r, rf_d_r, l_s_r, mapa_right, cancion_a_continuar, nombre_pista1, cant_predicciones_r)
-    p_conprediccion_l, v_conprediccion_l, d_conprediccion_l = predecir_cancion(mlp_p_l, rf_v_l, rf_d_l, l_s_l, mapa_left, cancion_a_continuar, nombre_pista2, cant_predicciones_l)
+    p_conprediccion_r, v_conprediccion_r, d_conprediccion_r = predecir_cancion_random(mlp_p_r, rf_v_r, rf_d_r, l_s_r, mapa_right, cant_predicciones_r,tempo_bpm)
+    p_conprediccion_l, v_conprediccion_l, d_conprediccion_l = predecir_cancion_random(mlp_p_l, rf_v_l, rf_d_l, l_s_l, mapa_left, cant_predicciones_l,tempo_bpm)
 
     cancion_nombre= 'cancion_generada_mlp.mid'
     cancion_generada = generar_cancion([[p_conprediccion_r, v_conprediccion_r, d_conprediccion_r],[p_conprediccion_l, v_conprediccion_l, d_conprediccion_l]], tempo_bpm,firma_de_compas,cancion_nombre)
--- RF.py
+++ RF.py
@@ -139,18 +139,65 @@ def predecir_cancion(rf_pitch, rf_velocity, rf_duration, longitud_secuencia, not
 
 
 
+import random
+
+def predecir_cancion_random(rf_pitch, rf_velocity, rf_duration, longitud_secuencia, notasyacordes, cant_predicciones,tempo):
+    # Generar valores iniciales aleatorios
+    duraciones_posibles = [i / 4 for i in range(5)]  # Genera [0, 0.25, 0.5, .]
+
+    # Generar valores iniciales aleatorios
+    todos_caracteristicas = [
+        [random.choice(notasyacordes) for _ in range(longitud_secuencia)],  # Pitches
+        [random.randint(0, 127) for _ in range(longitud_secuencia)],        # Velocities
+        [random.choice(duraciones_posibles) for _ in range(longitud_secuencia)],
+        tempo# Durations (en cuartos)
+    ]
+    
+        
+    #===== Para pitch =====
+    #transformo en indice de mapa:
+    for i, nota_acorde in enumerate(todos_caracteristicas[0]):
+        indice = notasyacordes.index(sorted(nota_acorde))
+        todos_caracteristicas[0][i] = indice
+    
+    # Predigo elementos
+    pitches_conprediccion = predecir_sig_elem_rf(todos_caracteristicas[0][0:longitud_secuencia],rf_pitch, cant_predicciones)
+    
+    # Vuelvo a transformar de índice a notas y acordes
+    for i in range(len(pitches_conprediccion)):
+        pitches_conprediccion[i] = notasyacordes[pitches_conprediccion[i]]
+    
+    
+    #===== Para velocities =====
+    print("\n====Prediccion de Velocities====")
+    velocities_conprediccion = predecir_sig_elem_rf(todos_caracteristicas[1][0:longitud_secuencia], rf_velocity, cant_predicciones)
+    
+    #===== Para durations =====
+    print("\n====Prediccion de Durations====")
+    #multiplico por 1000 y transformo en enteros
+    durations_originales = todos_caracteristicas[2][0:longitud_secuencia]
+    for i in range(len(durations_originales)):
+        durations_originales[i] *= 1000
+    durations_conprediccion = predecir_sig_elem_rf(durations_originales, rf_duration, cant_predicciones)
+    for i in range(len(durations_conprediccion)):
+        durations_conprediccion[i] /= 1000
+
+    return pitches_conprediccion, velocities_conprediccion, durations_conprediccion
+
+
+
 
 if __name__ == "__main__":
     from IPython import get_ipython
     get_ipython().magic('clear')
     
     #Segundos que se van a tomar para tomar secuencia
-    tiempo_secuencia=5
-    tiempo_a_predecir= 120 #debe ser mayor o igual a 2*tiempo_secuencia
+    # tiempo_secuencia=5
+    # tiempo_a_predecir= 120 #debe ser mayor o igual a 2*tiempo_secuencia
     
     c_a = "Audios/"
 
-    cancion_a_continuar = "AudiosAux/elise.mid"
+    cancion_a_continuar = "Audios/elise.mid"
 
     
     
@@ -160,17 +207,18 @@ if __name__ == "__main__":
     
     
 
-    l_s_r,l_s_l=calcular_longitud_secuencia(cancion_a_continuar, tiempo_secuencia,nombre_pista1,nombre_pista2)
+    # l_s_r,l_s_l=calcular_longitud_secuencia(cancion_a_continuar, tiempo_secuencia,nombre_pista1,nombre_pista2)
     
     tempo_bpm = getTempo(cancion_a_continuar)
     firma_de_compas = getTimeSignature(cancion_a_continuar)
 
 
-    cant_predicciones_r,cant_predicciones_l=calcular_longitud_secuencia(cancion_a_continuar, tiempo_a_predecir,nombre_pista1,nombre_pista2)
+    # cant_predicciones_r,cant_predicciones_l=calcular_longitud_secuencia(cancion_a_continuar, tiempo_a_predecir,nombre_pista1,nombre_pista2)
     
     
-    # l_s_r = 20
-    # l_s_l = 20
+    l_s_r = 15
+    l_s_l = 15
+    cantidad_predicciones =400
     
     # cant_predicciones_r = 234
     # cant_predicciones_l = 100
@@ -179,22 +227,69 @@ if __name__ == "__main__":
     print("\n=====Cargando acordes presentes en canciones=====")
     
     mapa_right, mapa_left = cargar_notas_acordes_canciones(c_a,nombre_pista1, nombre_pista2)
-    mapa_right,mapa_left=añadir_acordes_mapa(mapa_right,mapa_left,cancion_a_continuar,"right","left")
+    #mapa_right,mapa_left=añadir_acordes_mapa(mapa_right,mapa_left,cancion_a_continuar,"right","left")
     
     rf_p_r, rf_v_r, rf_d_r = inicializar_modelo(c_a,l_s_r, mapa_right, nombre_pista1)
     rf_p_l, rf_v_l, rf_d_l = inicializar_modelo(c_a,l_s_l, mapa_left, nombre_pista2)
     
-    p_conprediccion_r, v_conprediccion_r, d_conprediccion_r = predecir_cancion(rf_p_r, rf_v_r, rf_d_r, l_s_r, mapa_right, cancion_a_continuar, nombre_pista1, cant_predicciones_r)
-    p_conprediccion_l, v_conprediccion_l, d_conprediccion_l = predecir_cancion(rf_p_l, rf_v_l, rf_d_l, l_s_l, mapa_left, cancion_a_continuar, nombre_pista2, cant_predicciones_l)
+    p_conprediccion_r, v_conprediccion_r, d_conprediccion_r = predecir_cancion_random(rf_p_r, rf_v_r, rf_d_r, l_s_r, mapa_right, cantidad_predicciones,tempo_bpm)
+    p_conprediccion_l, v_conprediccion_l, d_conprediccion_l = predecir_cancion_random(rf_p_l, rf_v_l, rf_d_l, l_s_l, mapa_left, cantidad_predicciones,tempo_bpm)
+
+
+    
 
     cancion_nombre= 'cancion_generada_rf.mid'
-    cancion_generada = generar_cancion([[p_conprediccion_r[2*l_s_r:-1], v_conprediccion_r[2*l_s_r:-1], d_conprediccion_r[2*l_s_r:-1]],[p_conprediccion_l[2*l_s_l:-1], v_conprediccion_l[2*l_s_l:-1], d_conprediccion_l[2*l_s_l:-1]]], tempo_bpm,firma_de_compas,cancion_nombre)
+    cancion_generada = generar_cancion([[p_conprediccion_r[3*l_s_r:-1], v_conprediccion_r[3*l_s_r:-1], d_conprediccion_r[3*l_s_r:-1]],[p_conprediccion_l[3*l_s_l:-1], v_conprediccion_l[3*l_s_l:-1], d_conprediccion_l[3*l_s_l:-1]]], tempo_bpm,firma_de_compas,cancion_nombre)
     #cancion_generada.write('midi', fp='cancion_generada_rf.mid')
     
     fragmento_nombre='fragmento_rf.mid'
     fragmento = generar_cancion([[p_conprediccion_r[0:l_s_r], v_conprediccion_r[0:l_s_r], d_conprediccion_r[0:l_s_r]],[p_conprediccion_l[0:l_s_l], v_conprediccion_l[0:l_s_l], d_conprediccion_l[0:l_s_l]]], tempo_bpm,firma_de_compas,fragmento_nombre)
-   
     
+    
+    notas_a_tomar_r = 3*l_s_r
+    notas_a_tomar_l = 3*l_s_l
+    
+   
+    import os
+    
+    # Crear la carpeta "resultados_lstm" si no existe
+    directorio = "resultados_rf"
+    os.makedirs(directorio, exist_ok=True)
+    
+    # Crear una carpeta para los resultados si no existe
+    if not os.path.exists('resultados_rf'):
+        os.makedirs('resultados_rf')
+    
+    # Loop de 10 iteraciones para generar las canciones y fragmentos
+    for i in range(10):
+        # Predicción para la parte derecha de la canción
+        p_conprediccion_r, v_conprediccion_r, d_conprediccion_r = predecir_cancion_random(rf_p_r, rf_v_r, rf_d_r, l_s_r, mapa_right, cantidad_predicciones,tempo_bpm)
+
+        
+        # Predicción para la parte izquierda de la canción
+        p_conprediccion_l, v_conprediccion_l, d_conprediccion_l = predecir_cancion_random(rf_p_l, rf_v_l, rf_d_l, l_s_l, mapa_left, cantidad_predicciones,tempo_bpm)
+
+        
+        # Generar la canción completa
+        cancion_nombre = f"resultados_rf/cancion_generada_rf_{i+1}.mid"
+        cancion_generada = generar_cancion([[p_conprediccion_r[notas_a_tomar_r:-1],
+                                             v_conprediccion_r[notas_a_tomar_r:-1], 
+                                             d_conprediccion_r[notas_a_tomar_r:-1]],
+                                            [p_conprediccion_l[notas_a_tomar_l:-1], 
+                                             v_conprediccion_l[notas_a_tomar_l:-1], 
+                                             d_conprediccion_l[notas_a_tomar_l:-1]]], 
+                                           tempo_bpm,firma_de_compas,cancion_nombre)
+
+        
+        # Generar un fragmento de la canción
+        fragmento_nombre = f"resultados_rf/fragmento_rf_{i+1}.mid"
+        fragmento = generar_cancion(
+            [[p_conprediccion_r[:l_s_r], v_conprediccion_r[:l_s_r], d_conprediccion_r[:l_s_r]], 
+             [p_conprediccion_l[:l_s_l], v_conprediccion_l[:l_s_l], d_conprediccion_l[:l_s_l]]], 
+            tempo_bpm, firma_de_compas, fragmento_nombre)
+        
+        print(f"Generado {cancion_nombre} y {fragmento_nombre}") 
+   
    
     
    
